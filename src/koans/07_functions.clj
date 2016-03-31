6ef3defa68857351f391a116d7bb9083870f8e39
axiaoxin <254606826@qq.com>
2016-03-31 16:49:51 +0800

    07_functions is over

--- src/koans/07_functions.clj
+++ src/koans/07_functions.clj
@@ -42,8 +42,9 @@
   (= 20 ((fn [f] (f 4 5)) *))
 
   "Higher-order functions take function arguments"
-  (= 25 (___
+  (= 25 ((fn [f] (f 5))
           (fn [n] (* n n))))
 
   "But they are often better written using the names of functions"
-  (= 25 (___ square)))
+  (= 25 (#(% 5) square)))
+  ; (= 25 ((fn [f] (f 5)) square)))
