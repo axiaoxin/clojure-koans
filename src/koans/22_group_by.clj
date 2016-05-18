72fb7c48ad1b82cd4aaa6972d9916a1fd292b103
axiaoxin <254606826@qq.com>
2016-05-18 18:43:20 +0800

    22_group_by is over

--- src/koans/22_group_by.clj
+++ src/koans/22_group_by.clj
@@ -20,20 +20,22 @@
 ; ;;    2 [{:user-id 2, :uri "/foo"}]}
 
 (defn get-odds-and-evens [coll]
-  (let [{odds true evens false} (group-by __ coll)]
+  (let [{odds true evens false} (group-by (fn [x] (not= 0 (mod x 2))) coll)]
     [odds evens]))
 
 (meditations
   "To categorize a collection by some function, use group-by."
-  (= __ (group-by count ["hello" "world" "foo" "bar"]))
+  (= {3 ["foo" "bar"] 5 ["hello" "world"]} (group-by count ["hello" "world" "foo" "bar"]))
 
   "You can simulate filter + remove in one pass"
   (= (get-odds-and-evens [1 2 3 4 5])
-     ((juxt filter remove) odd? [1 2 3 4 5])
+     ((juxt filter remove) odd? [1 2 3 4 5])  ; --> [[1 3 5] [2 4]]
      [[1 3 5] [2 4]])
+  ; ((juxt a b c) x) => [(a x) (b x) (c x)]
 
   "You can also group by a primary key"
-  (= __
+  (= {1 [{:id 1 :name "Bob"} {:id 1 :last-name "Smith"}],
+      2 [{:id 2 :name "Jennifer"}]}
      (group-by :id [{:id 1 :name "Bob"}
                     {:id 2 :name "Jennifer"}
                     {:id 1 :last-name "Smith"} ]))
@@ -41,13 +43,14 @@
   "But be careful when you group by a non-required key"
   (= {"Bob" [{:name "Bob" :id 1}]
       "Jennifer" [{:name "Jennifer" :id 2}]
-      __ [{:last-name "Smith" :id 1}]}
+      nil [{:last-name "Smith" :id 1}]}
    (group-by :name [{:id 1 :name "Bob"}
                     {:id 2 :name "Jennifer"}
                     {:id 1 :last-name "Smith"}]))
 
   "The true power of group-by comes with custom functions"
-  (= __
+  (= {:naughty-list  [{:name  "Jimmy", :bad true}  {:name  "Joe", :bad true}],
+      :nice-list  [{:name  "Jane", :bad false}]}
      (group-by #(if (:bad %) :naughty-list :nice-list)
                [{:name "Jimmy" :bad true}
                 {:name "Jane" :bad false}
